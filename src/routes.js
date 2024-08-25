756cdadcfb8e2f431e07eb3d8502137a00642f10
EagleJhow <gamerjhon389@gmail.com>
2024-08-25 15:16:28 -0300

    arrumado

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from './pages/Home';
-import Login from './pages/Home/Login';
+import Login from './pages/Login';
 
 
 function RoutesApp(){
